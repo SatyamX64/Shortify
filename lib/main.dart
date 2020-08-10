67d222062464a07439d8f7625b8c8d6ba18f0be2
satyamx64 <satyamx64@gmail.com>
2020-08-10 14:38:15 +0530

    App Works but Future Builder was sacrificed to Remove Bug

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart' as path_provider;
 import 'Models/url.dart';
@@ -13,6 +12,7 @@ void main() async {
   final appDocumentDir = await path_provider.getApplicationDocumentsDirectory();
   Hive.init(appDocumentDir.path);
   Hive.registerAdapter(URLAdapter());
+  await Hive.openBox('urls');
   runApp(MyApp());
 }
 
@@ -37,30 +37,7 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       theme: themeData,
       debugShowCheckedModeBanner: false,
-      home: FutureBuilder(
-        future: Hive.openBox('urls'),
-        builder: (BuildContext context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            if (snapshot.hasError)
-              return Text(snapshot.error.toString());
-            else
-              return Scaffold(
-                body: HomePage(),
-              );
-          }
-          // Show Splash Screens before Box Opens
-          else
-            return Scaffold(
-              body: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Center(child: SizedBox(width: MediaQuery.of(context).size.width*0.6,child: SvgPicture.asset('icons/link.svg')),),
-                  Text('Shortify',style: TextStyle(color: Colors.black,fontWeight: FontWeight.w900,fontSize: 30),)
-                ],
-              ),
-            );
-        },
-      ),
+      home: Scaffold(body: HomePage(),),
     );
   }
 
